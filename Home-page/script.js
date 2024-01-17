e3513bc63defc6ac140842c883501995e3f334bb
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-01-17 14:09:14 -0300

    little updates

--- Home-page/script.js
+++ Home-page/script.js
@@ -1,1 +1,14 @@
 document.querySelector("#year").append(new Date().getFullYear());
+
+let images = ["staff1.jpg","staff2.jpg","staff3.jpg"];
+
+let firstimg = document.querySelector("#fir--img_test")
+let targetimg = document.querySelector("#sec--img_test")
+let thirdimg = document.querySelector("#thr--img_test")
+
+function changefirstimg(){
+    console.log(firstimg);
+}
+function changesecondimg(){
+
+}
